b2cba3c0127dc1904c0490c5cd04288e0f164b0e
oliver <oliver@172.16.13.129>
2020-04-21 16:24:55 -0400

    second push

---
+++ src/main/java/com/troyshoes/domain/User.java
@@ -0,0 +1,13 @@
+package com.troyshoes.domain;
+
+public class User {
+	private Long id;
+	private String username;
+	private String password;
+	private String firstname;
+	private String lastname;
+	
+	private String email;
+	private String phone;
+	private boolean eaabled=true;
+}
